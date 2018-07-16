43c737f8817bb2a5c8de3db0d59d4e4ea54cb5dc
Le Zhang <ZhangL@duerkopp-adler.com>
2018-07-16 16:33:12 +0200

    bind formModel

--- src/app/stock/stock-form/stock-form.component.ts
+++ src/app/stock/stock-form/stock-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Stock } from '../stock.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { StockService } from '../stock.service';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-stock-form',
@@ -10,7 +11,11 @@ import { StockService } from '../stock.service';
 })
 export class StockFormComponent implements OnInit {
 
+  formModel: FormGroup;
+
   stock: Stock;
+
+  categories = ['IT', 'Cloud', 'UI'];
 
   constructor(
     private routeInfo: ActivatedRoute,
@@ -21,6 +26,20 @@ export class StockFormComponent implements OnInit {
   ngOnInit() {
     let stockId = this.routeInfo.snapshot.params['id'];
     this.stock = this.stockService.getStock(stockId);
+
+    let fb = new FormBuilder();
+    this.formModel = fb.group(
+      {
+        name: [this.stock.name, [Validators.required, Validators.minLength(3)]],
+        price: [this.stock.price, [Validators.required]],
+        desc: [this.stock.desc],
+        categories: fb.array([
+          new FormControl(this.stock.categories.indexOf(this.categories[0]) !== -1),
+          new FormControl(this.stock.categories.indexOf(this.categories[1]) !== -1),
+          new FormControl(this.stock.categories.indexOf(this.categories[2]) !== -1)
+        ])
+      }
+    );
   }
 
   cancle() {
@@ -28,9 +47,22 @@ export class StockFormComponent implements OnInit {
   }
 
   save() {
-    console.log(this.stock.rating);
+    // console.log(this.stock.rating);
+
+    let englishCategories = [];
+    let index = 0;
+    for(let i = 0; i < 3; i++) {
+      if(this.formModel.value.categories[i]) {
+        englishCategories[index++] = this.categories[i];
+      }
+    }
+    this.formModel.value.categories = englishCategories;
+
+    this.formModel.value.rating = this.stock.rating;
+
+    console.log('formModel', this.formModel.value);
     
-    this.router.navigateByUrl('/stock');
+    // this.router.navigateByUrl('/stock');
   }
 
 }
